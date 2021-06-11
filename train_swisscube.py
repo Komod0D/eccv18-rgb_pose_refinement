1662e044e806b5d7d8fc0d09fd6f0da333dba663
Kimon Protopapas <kimon.protopapas@epfl.ch>
2021-06-11 04:22:57 +0200

    my inception

--- train_swisscube.py
+++ train_swisscube.py
@@ -5,7 +5,7 @@ import torch
 for sample in generate_samples():
     images, renders, ds, vs, poses_src, poses_tgt = sample
     print(images)
-    break
+    exit(0)
 
 network = load_network()
 params = [param for _, param in network.tune_parameters()]
